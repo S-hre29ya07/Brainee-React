0b5c3354ea75edeb867179f966117a1e19c02502
S-hre29ya07 <singh.shreya2907@gmail.com>
2023-03-25 23:35:16 +0530

    added static slider and added click handling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faAdd, faClapperboard,faRecordVinyl, faHome, faProjectDiagram, faFolder,faTrash,faCrown} from "@fortawesome/free-solid-svg-icons"
+
 const Sidebar = ({ changePage }) => {
   const [showCard, setShowCard] = useState(false);
   const [selectedButton, setSelectedButton] = useState(null);
+  const cardProjectRef = useRef(null);
+  const cardProjectInnerRef = useRef(null);
 
- const toggleCard = () => {
-  setShowCard(!showCard);
-  document.body.classList.toggle("overlay-active");
-};
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        cardProjectRef.current &&
+        cardProjectInnerRef.current &&
+        !cardProjectRef.current.contains(event.target) &&
+        !cardProjectInnerRef.current.contains(event.target)
+      ) {
+        setShowCard(false);
+        document.body.classList.remove("overlay-active");
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [cardProjectRef]);
 
+  const toggleCard = () => {
+    if (!showCard) {
+      setShowCard(true);
+      document.body.classList.add("overlay-active");
+    } else {
+      setShowCard(false);
+      document.body.classList.remove("overlay-active");
+    }
+  };
   const handlePageClick = (page) => {
     changePage(page);
     setSelectedButton(page);
@@ -17,17 +42,17 @@ const Sidebar = ({ changePage }) => {
 
   return (
     <aside className="sidebar">
-      <ul className="sidebar__list ">
+      <ul className="sidebar__list">
       <li className="sidebar__item admin__item">
       <div className="admin__container">
           <button className="admin__button">J</button>
           <span className="span-name">Jack Sparrow</span>
       </div>
         </li>
-        <li className="sidebar__item">
+        <li className="sidebar__item" ref={cardProjectRef}>
           <button className="new-project-button" onClick={toggleCard}><span>New Project</span><span><FontAwesomeIcon icon={faAdd}/></span></button>
           {showCard && (
-          <div className="card-project">
+          <div className="card-project" ref={cardProjectInnerRef}>
             <li className="sidebar__subitem">
               <button className="sidebar__subbutton"><span>Upload Files</span> <span><FontAwesomeIcon icon={faClapperboard}/></span></button>
             </li>
@@ -57,6 +82,7 @@ const Sidebar = ({ changePage }) => {
             <div className="free-plan">
               <h3 className="upgrade-heading">FREE PLAN</h3>
               <hr />
+              <input type="range" className="upgrade-range" min="-50" value="0"/>
               <button className="upgrade-button">UPGRADE TO PREMIUM <FontAwesomeIcon icon={faCrown} className="navbar__icon--margin"/></button>
             </div>
           </li>
