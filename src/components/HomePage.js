d19f967d0b8c030c9d7db3a79ae5e25dcd7a69f3
S-hre29ya07 <singh.shreya2907@gmail.com>
2023-03-28 16:09:12 +0530

    screen alignment for different browsers

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faSearch} from "@fortawesome/free-solid-svg-icons"
-import { faFolder } from "@fortawesome/free-solid-svg-icons";
+
 const HomePage = ({ currentPage }) => {
   let heading = "";
   switch (currentPage) {
